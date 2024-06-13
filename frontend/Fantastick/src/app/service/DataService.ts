7a4c440c5ea3b591fb0d7dcd653d10532ff0a55c
tronco99 <marco.zancanaro123@outlook.com>
2024-06-13 19:11:56 +0200

    connessione FE al BE e agguinta Model BONUS

--- frontend/Fantastick/src/app/service/DataService.ts
+++ frontend/Fantastick/src/app/service/DataService.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { BonusModel } from './models/BonusModel'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
 
   constructor(private http: HttpClient) { }
 
   getDato(nome: string): Observable<any> {
     return this.http.get<any>(`https://fantastick.adaptable.app/`).pipe(
-      tap((data: any) => {
+      tap((data: BonusModel) => {
+        console.log('Dati ricevuti:', data.IDLEGA);
+      }),
+      catchError(error => {
+        console.error('Errore durante la richiesta:', error);
+        return of(null); // Ritorna un observable con un valore di fallback
       })
     );
   }
---
+++ frontend/Fantastick/src/app/service/models/BonusModel.ts
@@ -0,0 +1,11 @@
+export interface BonusModel {
+    _id: string;
+    IDLEGA: string;
+    NPUNTEGGIO: number;
+    CTITOLO: string;
+    CDESCRIZIONE: string;
+    BCONTABILE: boolean;
+    NMASSIMO: number;
+    IDCREATORE: string;
+    DINSERIMENTO: string;
+  }
