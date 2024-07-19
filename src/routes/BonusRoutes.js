9ef357335a8bc9584c4c3234bc7fd51a921f50eb
tronco99 <marco.zancanaro123@outlook.com>
2024-07-19 11:51:18 +0200

    fix creazione lega

--- src/routes/BonusRoutes.js
+++ src/routes/BonusRoutes.js
@@ -35,42 +35,63 @@ router.post('/postCreaLega', async (req, res) => {
 
     const legaId = new ObjectId();
     const legheConNomeUguale = await legaService.getByCnome(nuovaLega.CNOME);
+    let nuoviBonusConId;
+    let nuoviGiovatoriConId;
 
     if(legheConNomeUguale.length == 0) {
-      const objectIdCreatore = ObjectId.createFromHexString(nuovaLega.IDCREATORE);
-      nuovaLega.LIDUSER = [nuovaLega.IDCREATORE];
-      nuovaLega.IDCREATORE = objectIdCreatore;
-      nuovaLega._id = legaId;
-  
-      const nuoviBonusConId = nuoviBonus.map(doc => ({
-        ...doc,
-        IDLEGA: legaId,
-        IDCREATORE: objectIdCreatore,
-        _id: new ObjectId()
-      }));
-  
-      const nuoviGiovatoriConId = nuoveCategorie.flatMap(doc =>
-        doc.giocatoriCollegati.map(giocatore => ({
-          ...giocatore,
-          _id: new ObjectId(),
-          IDCREATORE: objectIdCreatore
-        }))
-      );
-  
-      nuoveCategorie.forEach(categoria => {
-        categoria.LIDGIOCATORI = [];
-        categoria.giocatoriCollegati.forEach(gioc => {
-          const idGiocatoriCollegati = nuoviGiovatoriConId
-            .filter(giocatore => giocatore.CNOME === gioc.CNOME)
-            .map(giocatore => giocatore._id.toString());
-          categoria.LIDGIOCATORI.push(...idGiocatoriCollegati);
-        });
-      });
-  
-      const leghe = await legaService.aggiungiLega(nuovaLega, res);
-      const bonus = await bonusService.aggiungiBonus(nuoviBonusConId, res);
-      const categorie = await categorieService.aggiungiCategorie(nuoveCategorie, res);
-      const giocatori = await giocatoreService.aggiungiGiocatori(nuoviGiovatoriConId, res);
+      if(nuovaLega.CNOME) {
+        const objectIdCreatore = ObjectId.createFromHexString(nuovaLega.IDCREATORE);
+        nuovaLega.LIDUSER = [nuovaLega.IDCREATORE];
+        nuovaLega.IDCREATORE = objectIdCreatore;
+        nuovaLega._id = legaId;
+    
+        if(nuoviBonus.length > 0) {
+          nuoviBonusConId = nuoviBonus.map(doc => ({
+            ...doc,
+            IDLEGA: legaId,
+            IDCREATORE: objectIdCreatore,
+            _id: new ObjectId()
+          }));  
+        }
+       
+        if(nuoveCategorie.length > 0) {
+          nuoviGiovatoriConId = nuoveCategorie.flatMap(doc =>
+            doc.giocatoriCollegati.map(giocatore => ({
+              ...giocatore,
+              _id: new ObjectId(),
+              IDCREATORE: objectIdCreatore
+            }))
+          );
+      
+          nuoveCategorie.forEach(categoria => {
+            categoria.LIDGIOCATORI = [];
+            categoria.giocatoriCollegati.forEach(gioc => {
+              const idGiocatoriCollegati = nuoviGiovatoriConId
+                .filter(giocatore => giocatore.CNOME === gioc.CNOME)
+                .map(giocatore => giocatore._id.toString());
+              categoria.LIDGIOCATORI.push(...idGiocatoriCollegati);
+            });
+          });
+        }
+      }
+
+      let leghe;
+      let bonus;
+      let categorie;
+      let giocatori;
+
+      if(nuovaLega.CNOME) {
+        leghe = await legaService.aggiungiLega(nuovaLega, res);
+    
+        if(nuoviBonus.length > 0) {
+          bonus = await bonusService.aggiungiBonus(nuoviBonusConId, res);
+        }
+       
+        if(nuoveCategorie.length > 0) {
+          categorie = await categorieService.aggiungiCategorie(nuoveCategorie, res);
+          giocatori = await giocatoreService.aggiungiGiocatori(nuoviGiovatoriConId, res);
+        }
+      }      
   
       if(leghe || bonus || categorie || giocatori) {
         res.status(200).json({ status: 'success', message: 'Lega inserita' })
