7d2125733f91b6de79237851ff697c7ccf60066c
tronco99 <marco.zancanaro123@outlook.com>
2024-07-19 12:45:29 +0200

    fix

--- src/routes/BonusRoutes.js
+++ src/routes/BonusRoutes.js
@@ -37,24 +37,25 @@ router.post('/postCreaLega', async (req, res) => {
     const legheConNomeUguale = await legaService.getByCnome(nuovaLega.CNOME);
     let nuoviBonusConId;
     let nuoviGiovatoriConId;
+    let nGiocatori = [];
 
-    if(legheConNomeUguale.length == 0) {
-      if(nuovaLega.CNOME) {
-        const objectIdCreatore = ObjectId.createFromHexString(nuovaLega.IDCREATORE);
-        nuovaLega.LIDUSER = [nuovaLega.IDCREATORE];
-        nuovaLega.IDCREATORE = objectIdCreatore;
-        nuovaLega._id = legaId;
-    
-        if(nuoviBonus.length > 0) {
-          nuoviBonusConId = nuoviBonus.map(doc => ({
-            ...doc,
-            IDLEGA: legaId,
-            IDCREATORE: objectIdCreatore,
-            _id: new ObjectId()
-          }));  
-        }
-       
-        if(nuoveCategorie.length > 0) {
+    if (legheConNomeUguale.length == 0 && nuovaLega.CNOME) {
+      const objectIdCreatore = ObjectId.createFromHexString(nuovaLega.IDCREATORE);
+      nuovaLega.LIDUSER = [nuovaLega.IDCREATORE];
+      nuovaLega.IDCREATORE = objectIdCreatore;
+      nuovaLega._id = legaId;
+
+      if (nuoviBonus.length > 0) {
+        nuoviBonusConId = nuoviBonus.map(doc => ({
+          ...doc,
+          IDLEGA: legaId,
+          IDCREATORE: objectIdCreatore,
+          _id: new ObjectId()
+        }));
+      }
+
+      if (nuoveCategorie.length > 0) {
+        try {
           nuoviGiovatoriConId = nuoveCategorie.flatMap(doc =>
             doc.giocatoriCollegati.map(giocatore => ({
               ...giocatore,
@@ -62,9 +63,9 @@ router.post('/postCreaLega', async (req, res) => {
               IDCREATORE: objectIdCreatore
             }))
           );
-      
           nuoveCategorie.forEach(categoria => {
             categoria.LIDGIOCATORI = [];
+            nGiocatori.push(categoria.giocatoriCollegati.length)
             categoria.giocatoriCollegati.forEach(gioc => {
               const idGiocatoriCollegati = nuoviGiovatoriConId
                 .filter(giocatore => giocatore.CNOME === gioc.CNOME)
@@ -72,6 +73,8 @@ router.post('/postCreaLega', async (req, res) => {
               categoria.LIDGIOCATORI.push(...idGiocatoriCollegati);
             });
           });
+        } catch (err) {
+          res.status(500).json({ status: 'error', message: 'Categorie e giocatori non correttamente valorizzati' })
         }
       }
 
@@ -80,20 +83,25 @@ router.post('/postCreaLega', async (req, res) => {
       let categorie;
       let giocatori;
 
-      if(nuovaLega.CNOME) {
-        leghe = await legaService.aggiungiLega(nuovaLega, res);
-    
-        if(nuoviBonus.length > 0) {
-          bonus = await bonusService.aggiungiBonus(nuoviBonusConId, res);
-        }
-       
-        if(nuoveCategorie.length > 0) {
+    //  leghe = await legaService.aggiungiLega(nuovaLega, res);
+
+      if (nuoviBonus.length > 0) {
+        bonus = await bonusService.aggiungiBonus(nuoviBonusConId, res);
+      }
+
+      if (nuoveCategorie.length > 0 && !nGiocatori.includes(0)) {
+        try {
           categorie = await categorieService.aggiungiCategorie(nuoveCategorie, res);
           giocatori = await giocatoreService.aggiungiGiocatori(nuoviGiovatoriConId, res);
+        } catch (err) {
+          res.status(500).json({ status: 'error', message: 'Categorie e giocatori non correttamente valorizzati' })
         }
-      }      
-  
-      if(leghe || bonus || categorie || giocatori) {
+      } else if(nGiocatori.includes(0)) {
+        res.status(500).json({ status: 'error', message: 'Giocatori non inseriti' })
+        return 0
+      }
+
+      if (leghe || bonus || categorie || giocatori) {
         res.status(200).json({ status: 'success', message: 'Lega inserita' })
       } else {
         res.status(500).json({ status: 'error', message: 'Errore in fase di inserimento' });
