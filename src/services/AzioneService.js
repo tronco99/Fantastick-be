531ccbf2a379c9c5723eac53d65e25e8c1291893
tronco99 <marco.zancanaro123@outlook.com>
2024-07-30 18:10:58 +0200

    aggiutna bonusGiocatori

--- src/services/AzioneService.js
+++ src/services/AzioneService.js
@@ -47,154 +47,159 @@ class AzioneService {
     try {
       const objectId = ObjectId.createFromHexString(idLega);
 
-      const queryPerEstrarreIGiocatori = [
-        // Fase 1: Unione con la collezione "squadra"
-        {
-          $lookup: {
-            from: "SQUADRA",
-            localField: "IDLEGA",
-            foreignField: "IDLEGA",
-            as: "squadra_info"
-          }
-        },
-        { $unwind: "$squadra_info" },
-        {
-          $lookup: {
-            from: "USER",
-            localField: "squadra_info.IDUSER",
-            foreignField: "_id",
-            as: "user_info"
-          }
-        },
-        { $unwind: "$user_info" },
-        {
-          $addFields: {
-            "squadra_info.LIDGIOCATORIO": {
-              $map: {
-                input: "$squadra_info.LIDGIOCATORI",
-                as: "id",
-                in: { $toObjectId: "$$id" }
+      const queryPerEstrarreIGiocatori =
+        [
+          {
+            $lookup: {
+              from: "SQUADRA",
+              localField: "IDLEGA",
+              foreignField: "IDLEGA",
+              as: "squadra_info"
+            }
+          },
+          { $unwind: "$squadra_info" },
+          {
+            $lookup: {
+              from: "USER",
+              localField: "squadra_info.IDUSER",
+              foreignField: "_id",
+              as: "user_info"
+            }
+          },
+          { $unwind: "$user_info" },
+          {
+            $addFields: {
+              "squadra_info.LIDGIOCATORIO": {
+                $map: {
+                  input: "$squadra_info.LIDGIOCATORI",
+                  as: "id",
+                  in: { $toObjectId: "$$id" }
+                }
               }
             }
-          }
-        },
-        {
-          $lookup: {
-            from: "GIOCATORE",
-            localField: "squadra_info.LIDGIOCATORIO",
-            foreignField: "_id",
-            as: "giocatori_info"
-          }
-        },
-        { $unwind: "$giocatori_info" },
-        {
-          $lookup: {
-            from: "BONUS",
-            localField: "LAZIONI.IDBONUS",
-            foreignField: "_id",
-            as: "bonus_info"
-          }
-        },
-        { $unwind: "$bonus_info" },
-        // Aggiungi dettagli del bonus a LAZIONI
-        {
-          $unwind: "$LAZIONI"
-        },
-        {
-          $lookup: {
-            from: "BONUS",
-            localField: "LAZIONI.IDBONUS",
-            foreignField: "_id",
-            as: "bonus_info"
-          }
-        },
-        { $unwind: "$bonus_info" },
-
-        // Aggiungi dettagli del bonus a LAZIONI
-        {
-          $addFields: {
-            "LAZIONI.bonus": {
-              bonusId: "$bonus_info._id",
-              bonusTitolo: "$bonus_info.CTITOLO",
-              bonusDescrizione: "$bonus_info.CDESCRIZIONE",
-              bonusPunteggio: "$bonus_info.NPUNTEGGIO",
-              bonusQuantita: "$LAZIONI.NQUANTITA",
-              giocatoreId: "$IDGIOCATORE"
+          },
+          {
+            $lookup: {
+              from: "GIOCATORE",
+              localField: "squadra_info.LIDGIOCATORIO",
+              foreignField: "_id",
+              as: "giocatori_info"
             }
-          }
-        },
-
-        // Raggruppamento dei bonus per giocatore
-        {
-          $group: {
-            _id: {
-              giocatoreId: "$giocatori_info._id",
-              giocatoreNome: "$giocatori_info.CNOME",
-              giocatorePrezzo: "$giocatori_info.NPREZZO",
-              squadraNome: "$squadra_info.CNOME",
-              userNickName: "$user_info.CNICKNAME",
-              userId: "$user_info._id"
-            },
-            bonus: {
-              $push: "$LAZIONI.bonus"
+          },
+          { $unwind: "$giocatori_info" },
+          {
+            $lookup: {
+              from: "BONUS",
+              localField: "LAZIONI.IDBONUS",
+              foreignField: "_id",
+              as: "bonus_info"
             }
-          }
-        },
-
-        // Filtra i bonus nulli e raggruppa i bonus per giocatore
-        {
-          $addFields: {
-            bonus: {
-              $filter: {
-                input: "$bonus",
-                as: "item",
-                cond: { $ne: ["$$item.bonusId", null] }
+          },
+          { $unwind: "$bonus_info" },
+          {
+            $unwind: "$LAZIONI"
+          },
+          {
+            $lookup: {
+              from: "BONUS",
+              localField: "LAZIONI.IDBONUS",
+              foreignField: "_id",
+              as: "bonus_info"
+            }
+          },
+          { $unwind: "$bonus_info" },
+          {
+            $addFields: {
+              "LAZIONI.bonus": {
+                bonusId: "$bonus_info._id",
+                bonusTitolo: "$bonus_info.CTITOLO",
+                bonusDescrizione: "$bonus_info.CDESCRIZIONE",
+                bonusPunteggio: "$bonus_info.NPUNTEGGIO",
+                bonusQuantita: "$LAZIONI.NQUANTITA",
+                bonusGiocatore: { $multiply: ["$bonus_info.NPUNTEGGIO", "$LAZIONI.NQUANTITA"] },
+                giocatoreId: "$IDGIOCATORE"
               }
             }
-          }
-        },
-
-        // Raggruppamento per squadra e utente per incapsulare i giocatori
-        {
-          $group: {
-            _id: {
-              nomeSquadra: "$_id.squadraNome",
-              nicknameUser: "$_id.userNickName",
-              idUser: "$_id.userId"
-            },
-            giocatori: {
-              $push: {
-                _id: "$_id.giocatoreId",
-                CNOME: "$_id.giocatoreNome",
-                NPREZZO: "$_id.giocatorePrezzo",
-                bonus: {
-                  $filter: {
-                    input: "$bonus",
-                    as: "bonus",
-                    cond: { $eq: ["$$bonus.giocatoreId", "$_id.giocatoreId"] }
+          },
+          {
+            $group: {
+              _id: {
+                giocatoreId: "$giocatori_info._id",
+                giocatoreNome: "$giocatori_info.CNOME",
+                giocatorePrezzo: "$giocatori_info.NPREZZO",
+                squadraNome: "$squadra_info.CNOME",
+                userNickName: "$user_info.CNICKNAME",
+                userId: "$user_info._id"
+              },
+              bonus: {
+                $push: "$LAZIONI.bonus"
+              }
+            }
+          },
+          {
+            $addFields: {
+              bonus: {
+                $filter: {
+                  input: "$bonus",
+                  as: "item",
+                  cond: { $ne: ["$$item.bonusId", null] }
+                }
+              }
+            }
+          },
+          {
+            $group: {
+              _id: {
+                nomeSquadra: "$_id.squadraNome",
+                nicknameUser: "$_id.userNickName",
+                idUser: "$_id.userId"
+              },
+              giocatori: {
+                $push: {
+                  _id: "$_id.giocatoreId",
+                  CNOME: "$_id.giocatoreNome",
+                  NPREZZO: "$_id.giocatorePrezzo",
+                  bonus: {
+                    $filter: {
+                      input: "$bonus",
+                      as: "bonus",
+                      cond: { $eq: ["$$bonus.giocatoreId", "$_id.giocatoreId"] }
+                    }
                   }
                 }
               }
             }
+          },
+          {
+            $project: {
+              _id: 0,
+              nomeSquadra: "$_id.nomeSquadra",
+              nicknameUser: "$_id.nicknameUser",
+              idUser: "$_id.idUser",
+              giocatori: 1
+            }
           }
-        },
-
-        // Proiezione finale
-        {
-          $project: {
-            _id: 0,
-            nomeSquadra: "$_id.nomeSquadra",
-            nicknameUser: "$_id.nicknameUser",
-            idUser: "$_id.idUser",
-            giocatori: 1
-          }
-        }
-      ]
-      return await collection.aggregate(queryPerEstrarreIGiocatori).toArray();
+        ]
+      let data = await collection.aggregate(queryPerEstrarreIGiocatori).toArray();
+      return { azione: this.calculateBonus(data) };
     } catch (err) {
       console.log(err.message)
       res.status(500).send({ message: 'Estrazione fallita', error: err.message });
     }
+  }
+
+  calculateBonus(data) {
+    return data.map(squadra => {
+      const bonusGiocatori = squadra.giocatori.reduce((acc, giocatore) => {
+        const totalBonus = giocatore.bonus.reduce((sum, bonus) => sum + bonus.bonusGiocatore, 0);
+        return acc + totalBonus;
+      }, 0);
+
+      return {
+        ...squadra,
+        bonusGiocatori
+      };
+    });
   }
 
 }
